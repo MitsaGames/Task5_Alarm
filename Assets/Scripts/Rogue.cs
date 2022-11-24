8d1e21919e897eafd4661cd5d639ef11a0f59567
MitsaGames <MitsaGames@gmail.com>
2022-11-24 17:05:55 +0500

    Reaction to detection

--- Assets/Scripts/Rogue.cs
+++ Assets/Scripts/Rogue.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class Rogue : MonoBehaviour
 {
+    [SerializeField] private float _detectionResponseTime;
+
     public bool IsDetected { get; private set; } = false;
+    public RogueState State { get; private set; } = RogueState.RunningToTarget;
+
+    private Coroutine _coroutineWaitReactionToDetection;
+    private bool _isCoroutineWaitReactionToDetectionRunning = false;
+
+    private void Update()
+    {
+        if(State == RogueState.ReactionToDetection)
+        {
+            _coroutineWaitReactionToDetection = StartCoroutine(WaitReactionToDetection(_detectionResponseTime));
+            _isCoroutineWaitReactionToDetectionRunning = true;
+        }
+        else
+        {
+            if(_isCoroutineWaitReactionToDetectionRunning == true)
+            {
+                StopCoroutine(_coroutineWaitReactionToDetection);
+                _isCoroutineWaitReactionToDetectionRunning = false;
+            }
+        }
+    }
 
     public void OnDetected()
     {
+        State = RogueState.ReactionToDetection;
         IsDetected = true;
+    }
+
+    private IEnumerator WaitReactionToDetection(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+
+        State = RogueState.Detected;
     }
 }
--- Assets/Scripts/RogueMove.cs
+++ Assets/Scripts/RogueMove.cs
@@ -2,23 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rogue))]
+[RequireComponent(typeof(Rogue), typeof(Animator))]
 public class RogueMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _robberyTarget;
+    [SerializeField] private Transform _escapeTarget;
 
     private Rogue _rogue;
+    private Animator _animator;
 
     private void Awake()
     {
         _rogue = GetComponent<Rogue>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        var direction = _rogue.IsDetected? -1 : 1;
+        if(_rogue.State == RogueState.ReactionToDetection)
+        {
+            _animator.SetFloat("speed", 0);
+        }
+        else
+        {
+            _animator.SetFloat("speed", _speed);
+        }
 
-        transform.position = Vector3.MoveTowards(transform.position, _target.position, direction * _speed * Time.deltaTime);
+        if (_rogue.State == RogueState.RunningToTarget || _rogue.State == RogueState.ReactionToDetection)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _robberyTarget.position, _speed * Time.deltaTime);
+        }
+        else if (_rogue.State == RogueState.Detected)
+        {
+            var directionToEscape = _escapeTarget.position - transform.position;
+
+            transform.rotation = Quaternion.LookRotation(directionToEscape);
+
+            transform.position = Vector3.MoveTowards(transform.position, _escapeTarget.position, _speed * Time.deltaTime);
+        }
     }
 }
